4a2164b20a557dc65186e778ed02530e00de6453
fatima.abdelmonem <fatima.abdelmonem@gmail.com>
2017-05-21 15:46:04 +0200

    functionalities made

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
 
-namespace UnitTestProject1
+namespace test_selenium
 {
     [TestClass]
     public class UnitTest1
@@ -9,6 +11,24 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
+            ChromeDriver chrome = new ChromeDriver();
+            //navigate to ius system webpage
+            chrome.Navigate().GoToUrl("http://sis.ius.edu.ba/");
+            //enters id and password
+            chrome.FindElement(By.Id("txtLogin")).SendKeys("heregoesId");
+            chrome.FindElement(By.Id("txtPassword")).SendKeys("here thepassword");
+            //press login
+            chrome.FindElement(By.Id("btnLogin")).Click();
+            //opens the  course schedule
+            chrome.FindElement(By.ClassName("file")).Click();
+            //download the time table in pdf
+            chrome.FindElement(By.TagName("RptExportYaz_btnExportTo")).Click();
+            
+
+
+
+
         }
+
     }
 }
